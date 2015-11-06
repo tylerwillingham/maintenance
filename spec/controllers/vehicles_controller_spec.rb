a25a2faadc353a6ee236a6b0608c74ddf60a6c3e
Tyler Willingham <tylerwillingham@gmail.com>
2015-11-06 11:03:38 -0600

    Require authentication to access /vehicles

--- spec/controllers/vehicles_controller_spec.rb
+++ spec/controllers/vehicles_controller_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe VehiclesController, type: :controller do
+  it { should use_before_action(:require_login) }
+
   describe "GET #index" do
     it "assigns @vehicles" do
+      sign_in
       vehicle1 = create :vehicle
       vehicle2 = create :vehicle
 
@@ -11,6 +14,7 @@ RSpec.describe VehiclesController, type: :controller do
     end
 
     it "returns http success" do
+      sign_in
       get :index
       expect(response).to have_http_status(:success)
     end
@@ -18,11 +22,13 @@ RSpec.describe VehiclesController, type: :controller do
 
   describe "GET #new" do
     it "returns http succes" do
+      sign_in
       get :new
       expect(response).to have_http_status(:success)
     end
 
     it "assigns @vehicle to a new vehicle" do
+      sign_in
       get :new
       expect(assigns(:vehicle)).to be_instance_of(Vehicle)
     end
@@ -30,6 +36,7 @@ RSpec.describe VehiclesController, type: :controller do
 
   describe "POST #create" do
     it "creates a new vehicle" do
+      sign_in
       expect do
         post :create, vehicle: attributes_for(:vehicle)
       end.to change { Vehicle.count }.by(1)
@@ -37,6 +44,7 @@ RSpec.describe VehiclesController, type: :controller do
 
     context "a vehicle is created successfully" do
       it "redirects to vehicles#index" do
+        sign_in
         post :create, vehicle: attributes_for(:vehicle)
         expect(response).to redirect_to(vehicles_path)
       end
@@ -44,6 +52,7 @@ RSpec.describe VehiclesController, type: :controller do
 
     context "vehicle creation is unsuccessful" do
       it "redirects to vehicles#new" do
+        sign_in
         post :create, vehicle: attributes_for(:vehicle).except(:year)
         expect(response).to render_template(:new)
       end
